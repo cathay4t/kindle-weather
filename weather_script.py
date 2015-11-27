2ce3551dd7a016d7eb095b8b50dc2b048e45cd46
Gris Ge <cnfourt@gmail.com>
2015-11-27 14:21:53 +0800

    Add landscape mode.
    ./weather_script.py <key> 30.6586 104.0647 1
    
    Signed-off-by: Gris Ge <cnfourt@gmail.com>

--- weather_script.py
+++ weather_script.py
@@ -36,36 +36,46 @@ import os
 import sys
 
 from weather_api import WeatherAPI
+from argparse import ArgumentParser
 
 CODE_FOLDER = os.path.dirname(os.path.realpath(__file__))
 OUTPUT = "/var/www/html/weather/weather.png"
+SVG_PORTRAIT_FILE = "%s/weather-script-preprocess.svg" % CODE_FOLDER
+SVG_LANSCAPE_FILE = "%s/weather-script-preprocess-landscape.svg" % CODE_FOLDER
+SVG_FILE = SVG_PORTRAIT_FILE
+SVG_OUTPUT = "%s/weather-script-output.svg" % CODE_FOLDER
+MAX_WEATHER_DAY_COUNT = 3
 
-if len(sys.argv) != 4:
-    print("Need 3 argument for API key, latitude, longitud")
+if len(sys.argv) < 4:
+    print("Need 3 or more argument for API key, latitude, longitud, "
+          "[is_landscape]")
     exit(1)
 
 weather_obj = WeatherAPI(sys.argv[1], sys.argv[2], sys.argv[3])
 
+if len(sys.argv) >= 5 and sys.argv[4] != 0:
+    SVG_FILE = SVG_LANSCAPE_FILE
+
 # Open SVG to process
-output = codecs.open("%s/weather-script-preprocess.svg" % CODE_FOLDER, "r",
-                     encoding="utf-8").read()
+output = codecs.open(SVG_FILE, "r", encoding="utf-8").read()
 
 _MAP = {
-    "ICON": WeatherAPI.condition,
-    "HIGH": WeatherAPI.temp_max,
-    "LOW": WeatherAPI.temp_min,
+    "$I": WeatherAPI.condition,
+    "$H": WeatherAPI.temp_max,
+    "$L": WeatherAPI.temp_min,
 }
 
-_DAYS = ["ONE", "TWO", "THREE", "FOUR"]
-
 for x in _MAP.keys():
-    for i in range(len(_DAYS)):
-        output = output.replace("%s_%s" % (x, _DAYS[i]),
+    for i in range(MAX_WEATHER_DAY_COUNT + 1):
+        output = output.replace("%s%d" % (x, i),
                                 "%s" % _MAP[x](weather_obj, i))
 
 # Replace refresh time
-output = output.replace("TIME",
-                        datetime.datetime.now().strftime("%b %d %A %H:%M"))
+output = output.replace("$TIME",
+                        datetime.datetime.now().strftime("%b %d %a %H:%M"))
+
+# Updaet AQI. TODO(Gris Ge): still place holder yet.
+output = output.replace("$AQI", "Unknown")
 
 day_one = weather_obj.today
 
@@ -73,17 +83,14 @@ day_one = weather_obj.today
 one_day = datetime.timedelta(days=1)
 days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-for i in range(1, len(_DAYS)):
-    output = output.replace("DAY_%s" % _DAYS[i],
+for i in range(MAX_WEATHER_DAY_COUNT + 1):
+    output = output.replace("$D%s" % i,
                             days_of_week[(day_one + i * one_day).weekday()])
 
 # Write output
-codecs.open("%s/weather-script-output.svg" % CODE_FOLDER,
-            "w", encoding="utf-8").write(output)
+codecs.open(SVG_OUTPUT, "w", encoding="utf-8").write(output)
 
 os.system("rsvg-convert --background-color=white -o "
-          "%s %s/weather-script-output.svg"
-          % (OUTPUT, CODE_FOLDER))
+          "%s %s" % (OUTPUT, SVG_OUTPUT))
 
-os.system("pngcrush -c 0 -ow %s 1>/dev/null 2>&1" %
-          OUTPUT)
+os.system("pngcrush -c 0 -ow %s 1>/dev/null 2>&1" % OUTPUT)
